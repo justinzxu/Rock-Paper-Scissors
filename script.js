ac6eb1f7b34e9c476b439c3040e5133b85e1f68b
Justin Xu <justin.zhenchen.xu@gmail.com>
2021-12-13 00:56:51 -0800

    finished playround function

--- script.js
+++ script.js
@@ -1,2 +1,38 @@
 console.log("sup dude");
 
+function computerPlay(){
+    let computerResult=Math.floor(Math.random()*3);
+    if (computerResult==0){
+        return 'rock';
+    }
+    else if(computerResult==1){
+        return 'paper';
+    }
+    else {return 'scissors'};
+}
+
+console.log(computerPlay());
+
+let playerChoice=prompt("Enter Rock, Paper, or Scissors").toLowerCase();
+let computerChoice=computerPlay();
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection=="rock" && computerSelection=="paper"){
+       return console.log("I picked "+computerSelection+ " , you lose!")
+    }
+    else if (playerSelection=="paper" && computerSelection=="scissors"){
+        return console.log("I picked "+computerSelection+ " , you lose!")
+    }
+    else if (playerSelection=="scissors" && computerSelection=="rock"){
+        return console.log("I picked" +computerSelection+ " , you lose!")
+    }
+    else if (playerSelection==computerSelection){
+        return console.log("We both picked" + playerSelection + " , it's a tie!") 
+    }
+    else {
+        return console.log("I picked" +computerSelection + " and you picked" + playerSelection + " , you win!")
+    }
+}
+
+console.log(playRound(playerChoice, computerChoice));
+
