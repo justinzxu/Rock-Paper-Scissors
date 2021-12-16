9b2a9022c9c0d8a44c720d76f78562abb81905e2
Justin Xu <justin.zhenchen.xu@gmail.com>
2021-12-15 22:46:47 -0800

    finished writing functions for playing 5 rounds

--- script.js
+++ script.js
@@ -13,8 +13,26 @@ function computerPlay(){
 
 console.log(computerPlay());
 
-let playerChoice=prompt("Enter Rock, Paper, or Scissors").toLowerCase();
-let computerChoice=computerPlay();
+/*let playerChoice=prompt("Enter Rock, Paper, or Scissors").toLowerCase();
+let computerChoice=computerPlay(); */
+
+/* function playRound(playerSelection, computerSelection){
+    if (playerSelection=="rock" && computerSelection=="paper"){
+       return console.log("I picked "+computerSelection+ " , you lose!")
+    }
+    else if (playerSelection=="paper" && computerSelection=="scissors"){
+        return console.log("I picked "+computerSelection+ " , you lose!")
+    }
+    else if (playerSelection=="scissors" && computerSelection=="rock"){
+        return console.log("I picked " +computerSelection+ " , you lose!")
+    }
+    else if (playerSelection==computerSelection){
+        return console.log("We both picked " + playerSelection + " , it's a tie!") 
+    }
+    else {
+        return console.log("I picked " +computerSelection + " and you picked " + playerSelection + " , you win!")
+    }
+} */
 
 function playRound(playerSelection, computerSelection){
     if (playerSelection=="rock" && computerSelection=="paper"){
@@ -24,15 +42,27 @@ function playRound(playerSelection, computerSelection){
         return console.log("I picked "+computerSelection+ " , you lose!")
     }
     else if (playerSelection=="scissors" && computerSelection=="rock"){
-        return console.log("I picked" +computerSelection+ " , you lose!")
+        return console.log("I picked " +computerSelection+ " , you lose!")
     }
     else if (playerSelection==computerSelection){
-        return console.log("We both picked" + playerSelection + " , it's a tie!") 
+        return console.log("We both picked " + playerSelection + " , it's a tie!") 
     }
     else {
-        return console.log("I picked" +computerSelection + " and you picked" + playerSelection + " , you win!")
+        return console.log("I picked " +computerSelection + " and you picked " + playerSelection + " , you win!")
     }
 }
 
-console.log(playRound(playerChoice, computerChoice));
+function game(){
+    for (i=0; i<5; i++){
+    let playerChoice=prompt("Enter Rock, Paper, or Scissors").toLowerCase();
+    let computerChoice=computerPlay();
+    playRound(playerChoice, computerChoice);
+    }
+}
+
+game();
+
+/*bconsole.log(game(playerChoice, computerChoice));
+
+/* console.log(playRound(playerChoice, computerChoice)); */
 
