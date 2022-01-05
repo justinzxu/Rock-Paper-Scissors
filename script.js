4991d9ef40a27e0cb4421e5dba5e85a84cd51970
Justin Xu <justin.zhenchen.xu@gmail.com>
2022-01-05 01:29:47 -0800

    wrote html and css for website

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("sup dude");
-
 function computerPlay(){
     let computerResult=Math.floor(Math.random()*3);
     if (computerResult==0){
@@ -10,29 +8,6 @@ function computerPlay(){
     }
     else {return 'scissors'};
 }
-
-console.log(computerPlay());
-
-/*let playerChoice=prompt("Enter Rock, Paper, or Scissors").toLowerCase();
-let computerChoice=computerPlay(); */
-
-/* function playRound(playerSelection, computerSelection){
-    if (playerSelection=="rock" && computerSelection=="paper"){
-       return console.log("I picked "+computerSelection+ " , you lose!")
-    }
-    else if (playerSelection=="paper" && computerSelection=="scissors"){
-        return console.log("I picked "+computerSelection+ " , you lose!")
-    }
-    else if (playerSelection=="scissors" && computerSelection=="rock"){
-        return console.log("I picked " +computerSelection+ " , you lose!")
-    }
-    else if (playerSelection==computerSelection){
-        return console.log("We both picked " + playerSelection + " , it's a tie!") 
-    }
-    else {
-        return console.log("I picked " +computerSelection + " and you picked " + playerSelection + " , you win!")
-    }
-} */
 
 function playRound(playerSelection, computerSelection){
     if (playerSelection=="rock" && computerSelection=="paper"){
@@ -52,6 +27,7 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
+/*
 function game(){
     for (i=0; i<5; i++){
     let playerChoice=prompt("Enter Rock, Paper, or Scissors").toLowerCase();
@@ -62,7 +38,7 @@ function game(){
 
 game();
 
-/*bconsole.log(game(playerChoice, computerChoice));
+*/
 
-/* console.log(playRound(playerChoice, computerChoice)); */
+
 
